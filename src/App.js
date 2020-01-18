16e7f48734703eac08524683bd0b39f454675c93
Ian Kenley <ikenley6@gmail.com>
2020-01-18 11:18:38 -0500

    react-hooks why not

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
 const propTypes = {};
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <div className="py-5 px-3 text-center">
-          <div className="jumbotron">
-            <div className="container">
-              <h1>Open the pod bay doors, HAL</h1>
-              <p>Actual content coming soon</p>
-              <p>
-                <a
-                  className="btn btn-primary btn-lg"
-                  href="https://github.com/ikenley/moderatio"
-                  role="button"
-                  target="_blank"
-                >
-                  Learn more &raquo;
-                </a>
-              </p>
-            </div>
+const App = () => {
+  return (
+    <div className="container">
+      <div className="py-5 px-3 text-center">
+        <div className="jumbotron">
+          <div className="container">
+            <h1>Open the pod bay doors, HAL</h1>
+            <p>Actual content coming soon</p>
+            <p>
+              <a
+                className="btn btn-primary btn-lg"
+                href="https://github.com/ikenley/moderatio"
+                role="button"
+                target="_blank"
+              >
+                Learn more &raquo;
+              </a>
+            </p>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 App.propTypes = propTypes;
 export default App;
